0f1c64456365c9f2b888eb1b5b364b1b9d989bf0
Andrew Hansbrough <hansbroughku@gmail.com>
2020-03-24 14:34:54 -0500

    Added some suggested prompts sans loops, conditional statements etc. Added quotes around each item in the string for capLetters.

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,8 +1,33 @@
 // Assignment Code
-var capLetters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowLetters = ["abcdefghijklmnopqrstuvwxyz"]
-var specSymbols = ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"]
+var specSymbols = ["!\#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"]
 
+var c = [capLetters = 6]
+var l = [lowLetters = 6]
+var s = [specSymbols = 6]
+
+
+//Prompts Should Activate when User clicks "Generate Password"
+prompt("How many characters would you like to include in your password?") {
+  if confirm()
+  
+}
+//Capital Letters
+prompt("Would you Like to include capital letters?"){
+  if confirm()
+  if else 
+}
+//Lower-Case Letters
+prompt("Would you Like to include lower-case letters?") {
+  if confirm()
+  if else
+}
+//Special Characters
+prompt("Would you Like to include special characters?"){
+  if confirm()
+  if else
+}
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
