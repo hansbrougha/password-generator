681ac35e17a6e6f8e7b80eabe5d91cd43b22279c
Andrew Hansbrough <hansbroughku@gmail.com>
2020-03-26 23:55:36 -0500

    Office Hours: Split my characters sequence into their types, edited prompts mostly to confirms, added if statements to push strings to charBank.

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,33 +1,57 @@
 // Assignment Code
-var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var capLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowLetter = "abcdefghijklmnopqrstuvwxyz0123456789";
+var numbers = "1234567890";
+var specChar = "!$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-var password = " "
+var password = "";
 
-for(var i = 0; i <= complexity; i++){
-  password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
-  console.log()
+var totalChar = prompt("Choose Your Password Complexity: Please type a number between 8 and 128.");
+
+var wantsUpper = confirm("Would you Like to include upper-case letters?");
+
+var wantsLower = confirm("Would you like to include lower-case letters?");
+
+var wantsNumbers = confirm("Would you like to include numbers?");
+
+var wantsSpecial = confirm("Would you like to include special characters?");
+
+// Randomize Character sequence
+var charBank = [];
+if (wantsUpper === true) {
+  charBank.push(capLetter);
+}
+
+if (wantsLower === true) {
+  charBank.push(lowLetter);
+}
+
+if (wantsSpecial === true) {
+  charBank.push(specChar);
+}
+
+if (wantsNumbers === true) {
+  charBank.push(numbers);
+}
+console.log(charBank);
+
+//For Statement.
+for (var i = 0; i <= charBank; i++) {
+  password = password + values.charBank(Math.floor(Math.random() * Math.floor(value.length - 1)));
 
 }
 
-var complexity = prompt("Choose Your Password Complexity: Please type a number between 8 and 128.");
-
-var u = prompt ("Would you Like to include upper-case letters?");
-
-var l = prompt ("Would you like to include lower-case letters?");
-
-var s = prompt ("Would you like to include special characters?");
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
