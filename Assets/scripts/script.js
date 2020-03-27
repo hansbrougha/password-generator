99460b4fa1e2b1610c03754c3d721c937234efc8
Andrew Hansbrough <hansbroughku@gmail.com>
2020-03-27 00:11:21 -0500

    Added Comments for each established block of code, changed capLetters and lowLetters to upperCase and lowerCase (all occurences).

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,44 +1,52 @@
 // Assignment Code
-var capLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowLetter = "abcdefghijklmnopqrstuvwxyz0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz0123456789";
 var numbers = "1234567890";
 var specChar = "!$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 var password = "";
 
-var totalChar = prompt("Choose Your Password Complexity: Please type a number between 8 and 128.");
+//Prompts user to choose Password Complexity from 8-128.
+var totalChar = prompt("Choose Your Password Complexity: Please type a number between 8 and 128.");//What if a user selects smaller or larger numbers?
 
+//User chooses or rejects upper-case
 var wantsUpper = confirm("Would you Like to include upper-case letters?");
-
+//User Chooses or rejects lower-case
 var wantsLower = confirm("Would you like to include lower-case letters?");
-
+//User chooses or rejects numbers
 var wantsNumbers = confirm("Would you like to include numbers?");
-
+//User chooses or rejects special characters
 var wantsSpecial = confirm("Would you like to include special characters?");
 
 // Randomize Character sequence
 var charBank = [];
+
+//Pushes upperCase string to charbank.
 if (wantsUpper === true) {
-  charBank.push(capLetter);
+  charBank.push(upperCase);
 }
 
+//Pushes lowerCase string to charBank.
 if (wantsLower === true) {
-  charBank.push(lowLetter);
+  charBank.push(lowerCase);
 }
 
+//Pushes specChar string to charBank.
 if (wantsSpecial === true) {
   charBank.push(specChar);
 }
 
+//Pushes numbers string to charBank.
 if (wantsNumbers === true) {
   charBank.push(numbers);
 }
+
+//Validates contents of charBank.
 console.log(charBank);
 
-//For Statement.
+//For Loop. Random numbers from charBank up to number specified by user.
 for (var i = 0; i <= charBank; i++) {
   password = password + values.charBank(Math.floor(Math.random() * Math.floor(value.length - 1)));
-
 }
 
 // Assignment Code
