f446beda545ee16134e188137b2baf179df30fe7
Andrew Hansbrough <hansbroughku@gmail.com>
2020-03-29 12:51:33 -0500

    Added a generatePassword function, nested prompts, confirms, if statements within.

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -5,57 +5,53 @@ var numbers = "1234567890";
 var specChar = "!$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var password = "";
 
-//Prompts user to choose Password Complexity from 8-128.
-var totalChar = prompt("Choose Your Password Complexity: Please type a number between 8 and 128.");//What if a user selects smaller or larger numbers?
-console.log(totalChar);
+//Generate Password
+function generatePassword(){
+  //Prompts user to choose Password Complexity from 8-128.
+  var totalChar = prompt("Choose Your Password Complexity: Please type a number   between 8 and 128.");//What if a user selects smaller or larger numbers?
+  console.log(totalChar);
 
-//User chooses or rejects upper-case
-var wantsUpper = confirm("Would you Like to include upper-case letters?");
-//User Chooses or rejects lower-case
-var wantsLower = confirm("Would you like to include lower-case letters?");
-//User chooses or rejects numbers
-var wantsNumbers = confirm("Would you like to include numbers?");
-//User chooses or rejects special characters
-var wantsSpecial = confirm("Would you like to include special characters?");
+  //User chooses or rejects upper-case lower-case numbers special characters
+  var wantsUpper = confirm("Would you Like to include upper-case letters?");
+  var wantsLower = confirm("Would you like to include lower-case letters?");
+  var wantsNumbers = confirm("Would you like to include numbers?");
+  var wantsSpecial = confirm("Would you like to include special characters?");
 
-// Randomize Character sequence
-var charBank = []
+  // Randomize Character sequence
+  var charBank = []
+  if (totalChar < 8 || totalChar > 128)
+    prompt("Error: Please choose a valid complexity between 8 and 128 characters.");
 
-//Pushes upperCase string to charbank.
-if (wantsUpper === true) {
-  charBank.push(upperCase);
-}
+  if (wantsUpper === true) {
+    charBank.push(upperCase);
+  } //Pushes upperCase string to charbank.
+  if (wantsLower === true) {
+    charBank.push(lowerCase);
+  }//Pushes lowerCase string to charBank.
+  if (wantsSpecial === true) {
+    charBank.push(specChar);
+  } //Pushes specChar string to charBank.
+  if (wantsNumbers === true) {
+    charBank.push(numbers);
+  } //Pushes numbers string to charBank.
 
-//Pushes lowerCase string to charBank.
-if (wantsLower === true) {
-  charBank.push(lowerCase);
-}
+  //Validates contents of charBank.
+  console.log(charBank);
 
-//Pushes specChar string to charBank.
-if (wantsSpecial === true) {
-  charBank.push(specChar);
-}
-
-//Pushes numbers string to charBank.
-if (wantsNumbers === true) {
-  charBank.push(numbers);
-}
-
-//Validates contents of charBank.
-console.log(charBank);
 
 //For Loop. Random numbers from charBank up to number specified by user.
 for (var i = 0; i < charBank.length; i++) {
   password = password + charBank[Math.floor(Math.random() * charBank.length)];
 }
 console.log(password);
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
